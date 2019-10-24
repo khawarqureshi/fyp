bb64d0c6871f203a96e2515f858651dc8e9277b4
khawarqureshi <khawarqureshi08@gmail.com>
2019-10-24 20:13:56 +0500

    Fixed Home Styling :tada:

--- index.php
+++ index.php
@@ -16,85 +16,82 @@ else {
 <html>
 	<head>
 		<title>Welcome to Online Pharmacy</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 		<script src="/js/homeslideshow.js"></script>
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body style="min-width: 980px;">
-		<div class="homepageheader" style="position: relative;">
-			<div class="signinButton loginButton">
-				<div class="uiloginbutton signinButton loginButton" style="margin-right: 40px;">
-					<?php 
-						if ($user!="") {
-							echo '<a style="text-decoration: none; color: #fff;" href="logout.php">LOG OUT</a>';
-						}
-						else {
-							echo '<a style="color: #fff; text-decoration: none;" href="signin.php">SIGN UP</a>';
-						}
-					 ?>
-					
-				</div>
-				<div class="uiloginbutton signinButton loginButton" style="">
-					<?php 
-						if ($user!="") {
-							echo '<a style="text-decoration: none; color: #fff;" href="profile.php?uid='.$user.'">Hi '.$uname_db.'</a>';
-						}
-						else {
-							echo '<a style="text-decoration: none; color: #fff;" href="login.php">LOG IN</a>';
-						}
-					 ?>
-				</div>
-			</div>
-			<div style="float: left; margin: 5px 0px 0px 23px;">
-				<a href="index.php">
-					<img style=" height: 75px; width: 130px;" src="image/logo.png">
-				</a>
-			</div>
-			<div class="">
-				<div id="srcheader">
-					
-					  <!-- whatsapp code of two line below  -->
-			<?php
+	<nav class="navbar navbar-expand-lg navbar-light bg-light shadow">
+  <div class="container">
+  <a class="navbar-brand font-weight-bold" href="#">Online Pharmacy</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <form id="newsearch" class="form-inline my-2 my-lg-0" method="get" action="search.php">
+							<input type="text" class="form-control mr-sm-2" name="keywords"  maxlength="120"  placeholder="Search Here...">
+							<input type="submit" value="search" class="btn btn-outline-success" >	
+					</form>
+    <ul class="navbar-nav ml-auto">
+	<li class="ml-1">
+	  <?php 
+				if ($user!="") {
+					echo '<a class="btn btn-success" href="profile.php?uid='.$user.'">Hi '.$uname_db.'</a>';
+				}
+				else {
+					echo '<a class="btn btn-success" href="login.php">LOG IN</a>';
+				}
+				?>
+	  </li>
+	  <li class="ml-1">
+	  <?php
 				if ($user!="") {
 
-					echo '<a href="https://api.whatsapp.com/send?phone=03125544577"><img border="0" alt="whatsapp" src="https://www.aozata.com/wp-content/uploads/2019/01/iconfinder_whatsapp_287520.png" width="75" height="75"></a>';
+					echo '<a class="btn btn-primary" href="https://api.whatsapp.com/send?phone=03125544577">Watsapp</a>';
 				} else {
 
-					echo '<a href="signin.php"><img border="0" alt="whatsapp" src="https://www.aozata.com/wp-content/uploads/2019/01/iconfinder_whatsapp_287520.png" width="75" height="75"></a>';
+					echo '<a href="signin.php" class="btn btn-primary">Watsapp</a>';
 				}
-			?>
-					
-					
-					
-					<form id="newsearch" method="get" action="search.php">
-					        <input type="text" class="srctextinput" name="keywords" size="21" maxlength="120"  placeholder="Search Here..."><input type="submit" value="search" class="srcbutton" >
-						
-						<!-- My Code -->
-						
-							<input type="submit" value="Upload Prescription" class="srcbutton" >
-					</form>
-				<div class="srcclear"></div>
+?>
+	  </li>
+	  <li class="nav-item ml-1">
+				<?php 
+				if ($user!="") {
+					echo '<a class="btn btn-primary" href="logout.php">LOG OUT</a>';
+				}
+				else {
+					echo '<a class="btn btn-outline-primary" href="signin.php">SIGN UP</a>';
+				}
+				?>
+	  </li>
+	  <li>
+	</ul>
 				
-				</div>
-			</div>
-		</div>
-		<div class="home-welcome">
-			<div class="home-welcome-text" style="background-image: url(image/homebackgrndimg.png); height: 380px; ">
-				<h1 style="margin: 0px;">Welcome To Pharmacy</h1>
-				<h2>Largest Online Pharmacy In Pakistan</h2>
-			</div>
-		</div>
+  </div>
+  </div>
+</nav>
+			
+<div class="jumbotron jumbotron-fluid ">
+  <div class="container text-center">
+	<h1 class="display-4">Welcome to <span class="text-success"> Online Pharmacy</span></h1>
+	<hr class="bg-success"/>
+    <p class="lead alert alert-success fit-content">Pakistan No. 1 <span class='text-primary font-weight-bold'>Pharmacy</span> Website.</p>
+  </div>
+</div>
 		<div class="home-prodlist">
 			<div>
-				<h3 style="text-align: center;">Products Category</h3>
+				<h3 class="text-center font-weight-bold text-info">Products Category</h3>
+				<hr/>
 			</div>
-			<div style="padding: 20px 30px; width: 85%; margin: 0 auto;">
+			<div class="container">
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/Headache.php">
-							<img src="./image/product/Headache/HeadImage.jpg" class="home-prodlist-imgi">
+							<img src="./image/product/Headache/HeadImage.jpg" class="img-thumbnail">
 							</a>
 						</div>
 					</li>
@@ -102,7 +99,7 @@ else {
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/Nutritional.php">
-							<img src="./image/product/Nutritional/NutritionImage.png" class="home-prodlist-imgi">
+							<img src="./image/product/Nutritional/NutritionImage.png" class="img-thumbnail">
 							</a>
 						</div>
 					</li>
@@ -110,42 +107,42 @@ else {
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/Orthopedic.php">
-							<img src="./image/product/Orthopedic/OrthoImage.jpg" class="home-prodlist-imgi"></a>
+							<img src="./image/product/Orthopedic/OrthoImage.jpg" class="img-thumbnail"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/EyeInfection.php">
-							<img src="./image/product/EyeInfection/EyeInfect.jpg" class="home-prodlist-imgi"></a>
+							<img src="./image/product/EyeInfection/EyeInfect.jpg" class="img-thumbnail"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/Nausea.php">
-							<img src="./image/product/Nausea/NauseaImage.jpg" class="home-prodlist-imgi"></a>
+							<img src="./image/product/Nausea/NauseaImage.jpg" class="img-thumbnail"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/Allergy.php">
-							<img src="./image/product/Allergy/AlergyImage.jpg" class="home-prodlist-imgi"></a>
+							<img src="./image/product/Allergy/AlergyImage.jpg" class="img-thumbnail"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/Infection.php">
-							<img src="./image/product/Infection/infectImage.jpg" class="home-prodlist-imgi"></a>
+							<img src="./image/product/Infection/infectImage.jpg" class="img-thumbnail"></a>
 						</div>
 					</li>
 				</ul>
 				<ul style="float: left;">
 					<li style="float: left; padding: 25px;">
 						<div class="home-prodlist-img"><a href="category/Depression.php">
-							<img src="./image/product/Depression/depImage.jpg" class="home-prodlist-imgi"></a>
+							<img src="./image/product/Depression/depImage.jpg" class="img-thumbnail"></a>
 						</div>
 					</li>
 				</ul>
